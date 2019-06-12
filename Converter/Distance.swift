0739a6cfbce6711d3719ab5bbf5dd9751de63362
Santiago <santiagoj@unicauca.edu.co>
2019-06-11 19:28:19 -0500

    Initial Commit

---
+++ Converter/Distance.swift
@@ -0,0 +1,35 @@
+//
+//  Distance.swift
+//  Converter
+//
+//  Created by Enfasis on 6/11/19.
+//  Copyright © 2019 Francisco Martinez. All rights reserved.
+//
+import UIKit
+//: # Distance class
+//:
+class Distance {
+    static let kmPerMile = 1.60934
+    
+    var miles:Double
+    var km:Double {
+        get {
+            return Distance.toKm(miles: miles)
+        }
+        set(newKm) {
+            miles = Distance.toMiles(km: newKm)
+        }
+    }
+    init(miles:Double) {
+        self.miles = miles
+    }
+    init(km:Double) {
+        self.miles = Distance.toMiles(km: km)
+    }
+    static func toKm(miles:Double)->Double {
+        return miles * kmPerMile
+    }
+    static func toMiles(km:Double)->Double {
+        return km / kmPerMile
+    }
+}
---
+++ Converter/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Converter
+//
+//  Created by Francisco Martinez on 6/8/19.
+//  Copyright © 2019 Francisco Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //Object of model class Distance
+    var distance = Distance(miles: 1000)
+
+    @IBOutlet weak var milesTextField: UITextField!
+    @IBOutlet weak var kmTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //Set values from model class
+        milesTextField.text = "\(distance.miles)"
+        kmTextField.text = "\(distance.km)"
+    }
+
+    @IBAction func convertToKm(_ sender: Any) {
+        if let miles = Double(milesTextField.text!) {
+            distance.miles = miles
+            kmTextField.text = "\(Int(distance.km))"
+        }
+    }
+    
+    @IBAction func convertToMiles(_ sender: Any) {
+        if let km = Double(kmTextField.text!) {
+            distance.km = km
+            milesTextField.text = "\(Int(distance.miles))"
+        }
+    }
+}
+
